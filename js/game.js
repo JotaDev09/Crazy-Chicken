3a245f824210d81f73a52e3298da33afd598c5ad
Juan de Santos <juanj.desantos@gmail.com>
2022-12-02 19:03:33 +0100

    save files

---
+++ js/game.js
@@ -0,0 +1,13 @@
+let canvas;
+let ctx;
+let world;
+
+function init() {
+    
+    canvas = document.getElementById('canvas');
+    world = new World(canvas);
+    //ctx = canvas.getContext('2d');
+
+   console.log('My Character is', world.character);
+    
+}
